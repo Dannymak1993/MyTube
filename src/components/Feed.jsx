25fc27be19803004f6f8c349c4b319ea472a16df
danny-mak1993 <danny.mak2@gmail.com>
2023-09-04 16:12:47 -0500

    added elements to feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
 
 function Feed() {
-  return (
-    <div>Feed</div>
-  )
+  return <div>Feed</div>;
 }
 
-export default Feed
+export default Feed;
