056c665760bd6a9eee1b84d3be4c8818c62c6f29
danny-mak1993 <danny.mak2@gmail.com>
2023-09-04 16:17:58 -0500

    added elements to channeldetail component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -5,8 +5,26 @@ import { Box } from "@mui/material";
 import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
-function ChannelDetail() {
-  return <div>ChannelDetail</div>;
-}
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+
 
 export default ChannelDetail;
