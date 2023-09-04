2e3b0f21217b6c89aad716c16815ae4c303a08c2
danny-mak1993 <danny.mak2@gmail.com>
2023-09-04 16:02:14 -0500

    added searchbar elements

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
 
   const onhandleSubmit = (e) => {
@@ -13,6 +13,37 @@ const SearchBar = () => {
     if (searchTerm) {
       navigate(`/search/${searchTerm}`);
 
-      setSearchTerm('');
+      setSearchTerm("");
     }
   };
+  
+  return (
+    <Paper
+      component="form"
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
