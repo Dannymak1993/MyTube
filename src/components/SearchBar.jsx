fa90e98961dd5469a8984dc117a037ce46392853
danny-mak1993 <danny.mak2@gmail.com>
2023-09-04 16:01:49 -0500

    added searchbar elements

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,3 +2,17 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
