60bbe20ff7d194930016c82db6dda90809a20101
danny-mak1993 <danny.mak2@gmail.com>
2023-09-04 16:13:21 -0500

    added elements to feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,8 +4,15 @@ import { Box, Stack, Typography } from "@mui/material";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Videos, Sidebar } from "./";
 
-function Feed() {
-  return <div>Feed</div>;
-}
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
 
 export default Feed;
